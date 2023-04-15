4d2d201bf40a7a0dfb7274b17e3778c9dc63de96
Sameer Anand <sameer@spawned.io>
2023-04-14 20:27:17 -0400

    Updates

---
+++ precache-manifest.14379fe22d6b310beeb718a4ebb3f85a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6985394b5664e1350b01e932af2d2574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2bd4a8467916bf79456",
+    "url": "/static/css/main.4f0b127b.chunk.css"
+  },
+  {
+    "revision": "03e0939d5e581106011f",
+    "url": "/static/js/2.468c6cff.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.468c6cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bd4a8467916bf79456",
+    "url": "/static/js/main.04ff30b8.chunk.js"
+  },
+  {
+    "revision": "ee3fcdb6d1a49310f008",
+    "url": "/static/js/runtime-main.b4161b0e.js"
+  }
+]);
